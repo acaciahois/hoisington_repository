a7df5c20148079568475f285e2e220af6d228895
David-Collins <dave847@gmail.com>
2013-07-25 16:17:18 -0400

    hello

---
+++ FINALProject1/finalproject1/src/finalproject1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class finalproject1 
+{
+	public static void main (String []args)
+	{
+		System.out.println("Welcome to Acacia's Game: Movie Quote Trivia")
+		//dont know if i want to include this: System.out.println("The rules are fairly simple.  The computer will give you a movie quote and you must guess the movie it's from.")
+		System.out.println("Would you like to begin?")
+		
+		
+		ArrayList<String>
+	}
+}
---
+++ FinalProject/FinalProject/src/finalproject.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+public class finalproject {
+	public static void main (String []args)
+	{
+		Scanner scan = new Scanner(System.in);
+		system.out.println("Choose a category: normal, insults, movie quotes")
+		/*switch {
+		case normal
+			stuff;
+		case insults
+			stuff;
+		case movie quotes
+		 	stuff;
+		 	*/
+		
+		public String[] normalphr = {"Hello","Nice to meet you","My name is Rafiki","I like apple pie","My favorite hobby is scuba diving.  I know what you're thinking: A computer that dives? But it can be done!",
+		}
+		public String[] moviephr = {"Get to zhe Choppa!","MUFASA!!! Just saying his name makes me shiver.","It's ... PEOPLE!","Get your hands off me you DAMN, DIRTY APES!",""
+		for x = Keyboardclass
+				random.shuffle (normalphr)
+		
+		
+		
+		/*public static KeyStroke getKeyStroke
+		 KeyStroke keStroke = KeyStroke.getKeyStroke("typed A");
+
+		Action act = new MyActionListener("Action occured!");
+		
+		java.awt.event.KeyEvent.VK_ENTER
+		java.awt.event.KeyEvent.VK_TAB
+		java.awt.event.KeyEvent.VK_SPACE
+		
+		  // add CTRL-L: select current line (action provided for us)
+    KeyStroke L = KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_MASK);
+    Action actionL = (Action)lookup.get(DefaultEditorKit.selectLineAction);
+    newmap.addActionForKeyStroke(L, actionL);
+		 */
+	}
+
+	
+}
