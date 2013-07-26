45903c99750550f5eb7a7b8dd247c012450998c3
David-Collins <dave847@gmail.com>
2013-07-26 13:20:34 -0400

    hiya

--- FINALProject1/finalproject1/src/finalproject1.java
+++ FINALProject1/finalproject1/src/finalproject1.java
@@ -8,17 +8,17 @@ public class finalproject1
 	{
 		System.out.println("Welcome to Acacia's Game: Movie Quote Trivia");
 		// don't know if i want to include this: System.out.println("The rules are fairly simple.  The computer will give you a movie quote and you must guess the movie it's from.")
-		System.out.println("Would you like to begin?");
+		System.out.println("Let's begin.");
 		
 		Scanner userInput = new Scanner (System.in);
-		System.out.println("1." + "Get to zhe Choppa!");
+		System.out.println("1." + "'Get to zhe Choppa!'");
 		String input = userInput.next();
 			if (input == "Predator")
 			{
 				System.out.println("Correct!");
 				
 			}
-			else (input != "Predator") 
+			else
 			{
 				System.out.println("Incorrect.  Would you like to try again or do you give up?");
 					if(input == "Try again") //NEED TO WORK ON
